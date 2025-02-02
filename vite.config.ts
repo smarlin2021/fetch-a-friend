77367220c5122d4bd1db8eb83ace068b40b770bf
Sierra Marlin <85563753+smarlin2021@users.noreply.github.com>
2025-02-01 21:43:50 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    cors: { origin: 'https://frontend-take-home-service.fetch.com'},
+  },
   base: "/fetch-a-friend/",
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    manifest: true,
   }
 });
