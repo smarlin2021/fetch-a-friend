1d4863cb10a2c0d96dc54fca45496cbc077f3050
Sierra Marlin <85563753+smarlin2021@users.noreply.github.com>
2025-02-01 20:42:24 -0500

    fixing redirection issue

--- src/data/APIConnection.tsx
+++ src/data/APIConnection.tsx
@@ -52,7 +52,7 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      window.location.href = '/login';
+      window.location.href = '/fetch-a-friend/login';
     }
     return Promise.reject(error);
   }
