fe01699111e0188bc7b263b58942154f547196ab
Sierra Marlin <85563753+smarlin2021@users.noreply.github.com>
2026-09-11 09:17:37 -0400

    Fix GitHub Pages routing and deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import '@mantine/core/styles.css';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
 import { MantineProvider } from '@mantine/core';
+
 import { AdoptionsPage } from './pages/Adoptions.page';
 import FavoritesPage from './pages/Favorites.page';
 import { HomePage } from './pages/Home.page';
 import LoginPage from './pages/Login.page';
 import { theme } from './theme';
 
-const basename = import.meta.env.BASE_URL || '/fetch-a-friend';
-
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <BrowserRouter basename={basename}>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/dogs" element={<AdoptionsPage />} />
           <Route path="/favorites" element={<FavoritesPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </MantineProvider>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+
   server: {
-    cors: { origin: 'https://frontend-take-home-service.fetch.com'},
+    cors: {
+      origin: 'https://frontend-take-home-service.fetch.com',
+    },
   },
-  base: "/fetch-a-friend/",
+
+  base: '/fetch-a-friend/',
+
   build: {
     outDir: 'dist',
     manifest: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        '404': resolve(__dirname, 'public/404.html')
-      }
-    }
   },
+
   resolve: {
     alias: {
       '@': '/src',
